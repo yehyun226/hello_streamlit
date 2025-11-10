d68e366ca9ec457809f0014550832b2559a4bbc7
yehyun226 <1361015syh226@gmail.com>
2025-11-10 19:35:53 +0900

    Delete Streamlit directory

--- Streamlit/columns_layout_4_streamlit.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.sidebar.write("This lives in the side")
-c = st.sidebar.button("Clicked me")
-print(c)
--- Streamlit/columns_layout_5_streamlit.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-tab1, tab2 = st.tabs(["Tab1", "Tab2"])
-tab1.write("this is tab1")
-tab2.write("this is tab2")
